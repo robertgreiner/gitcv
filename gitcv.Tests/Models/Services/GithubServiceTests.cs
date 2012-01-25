87793b8763fe41bee0704e499d1cbc69977e20a4
Robert Greiner <robert@robertgreiner.com>
2012-01-25 15:59:57 -0600

    Successfully connecting to the Github API

---
+++ gitcv.Tests/Models/Services/GithubServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NUnit.Framework;
+
+namespace gitcv.Tests.Models.Services
+{
+    [TestFixture]
+    class GithubServiceTests
+    {
+        [Test]
+        public void ShouldReturnSomethingValidWhenMakingARequestToGithubApi()
+        {
+            var request = WebRequest.Create("https://github.com/api/v2/json/user/search/robertgreiner") as HttpWebRequest;
+            var response = request.GetResponse() as HttpWebResponse;
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
