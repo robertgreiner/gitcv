877017a678f51a58bb633485996c393d0c264797
Robert Greiner <robert@robertgreiner.com>
2012-01-25 16:07:17 -0600

    Get response from API when using a valid github username.

--- gitcv.Tests/Models/Services/GithubServiceTests.cs
+++ gitcv.Tests/Models/Services/GithubServiceTests.cs
@@ -11,11 +11,21 @@ namespace gitcv.Tests.Models.Services
     class GithubServiceTests
     {
         [Test]
-        public void ShouldReturnSomethingValidWhenMakingARequestToGithubApi()
+        public void ShouldReturnEmptyContentOnInvalidGithubUsername()
+        {
+            var request = WebRequest.Create("https://github.com/api/v2/json/user/search/alksjhflakjsflkajsflkajsa12476214") as HttpWebRequest;
+            var response = request.GetResponse() as HttpWebResponse;
+            response.Close();
+            Assert.AreEqual(12, response.ContentLength);
+        }
+
+        [Test]
+        public void ShouldReturnUserSpecificContentOnValidGithubUsername()
         {
             var request = WebRequest.Create("https://github.com/api/v2/json/user/search/robertgreiner") as HttpWebRequest;
             var response = request.GetResponse() as HttpWebResponse;
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            response.Close();
+            Assert.IsTrue(response.ContentLength > 12);
         }
     }
 }
