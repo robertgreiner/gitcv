ef6cd52a6c5be2c6683c76855eb8d9ba07cc66a2
Robert Greiner <robert@robertgreiner.com>
2012-01-26 20:35:28 -0600

    Get user JSON from github API and populate an object with it.

--- gitcv.Tests/Models/Services/GithubServiceTests.cs
+++ gitcv.Tests/Models/Services/GithubServiceTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using NUnit.Framework;
+using System.Runtime.Serialization.Json;
 
 namespace gitcv.Tests.Models.Services
 {
@@ -29,8 +30,46 @@ namespace gitcv.Tests.Models.Services
             var receiveStream = response.GetResponseStream();
             var readStream = new StreamReader(receiveStream, Encoding.UTF8);
             var stream = readStream.ReadToEnd();
-            response.Close();
+            response.Close();   
             Assert.IsTrue(stream.Contains("\"login\":\"robertgreiner\""));
         }
+
+        [Test]
+        public void ShouldReturnPublicRepoCount()
+        {
+            var request = WebRequest.Create("https://api.github.com/users/robertgreiner") as HttpWebRequest;
+            var response = request.GetResponse() as HttpWebResponse;
+            var receiveStream = response.GetResponseStream();
+
+            var serializer = new DataContractJsonSerializer(typeof(GithubUser));
+            var user = (GithubUser) serializer.ReadObject(receiveStream);
+
+            Assert.AreEqual("robertgreiner", user.login);
+
+            response.Close();
+        }
+    }
+
+    public class GithubUser
+    {
+        public String login { get; set; }
+        public String id { get; set; }
+        public String avatar_url { get; set; }
+        public String gravatar_id { get; set; }
+        public String url { get; set; }
+        public String name { get; set; }
+        public String company { get; set; }
+        public String blog { get; set; }
+        public String location { get; set; }
+        public String email { get; set; }
+        public String hireable { get; set; }
+        public String bio { get; set; }
+        public String public_repos { get; set; }
+        public String public_gists { get; set; }
+        public String followers { get; set; }
+        public String following { get; set; }
+        public String html_url { get; set; }
+        public String created_at { get; set; }
+        public String type { get; set; }
     }
 }
